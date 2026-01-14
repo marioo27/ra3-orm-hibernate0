95ebfdb9d35fe8ee996504817d6a2c303264fc7d
Mario Garcia <mariogarrod@gmail.com>
2026-01-14 09:34:59 +0100

    conexion h2

--- src/main/java/es/ciudadescolar/Main.java
+++ src/main/java/es/ciudadescolar/Main.java
@@ -1,7 +1,50 @@
 package es.ciudadescolar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.ciudadescolar.instituto.Alumno;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("PersistenciaInstituto");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EntityManager em = null;
+        EntityTransaction trans = null;
+
+        Alumno al = new Alumno("Juan", "juan@ciudadescolar.es");
+        try {
+            em = EMF.createEntityManager();
+            trans = em.getTransaction();
+            // Siempre necesito una transaccion para modificar la BD (altas, bajas y
+            // modificaciones)
+
+            trans.begin();
+
+            em.persist(al);
+            LOG.debug("La instancia de alumno pasa a estar administrada o persistente");
+
+            trans.commit();
+            // A partir de aqui (porque se ha cerrado la sesion) la instancia pasa a estar
+            // separada (detached) Cambios posteriores no se vuelcana a la BD
+
+        } catch (RuntimeException e) {
+            // Las excepciones propagadas por motivos de persistencia extienden de Runtime
+            // Exception (no poner Exception), dichas excepciones son NO comprobadas
+            LOG.error("Error durante la persistencia de datos [" + e.getMessage() + "]");
+            trans.rollback();
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+                LOG.debug("Se ha cerrado la conexion con la BD");
+            }
+        }
+
     }
 }
